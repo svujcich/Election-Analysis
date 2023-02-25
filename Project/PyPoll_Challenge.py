7ea3a0a3122b26f04938d6901d836b45a7238ea9
Sarah <sarah.vujcich.school@gmail.com>
2023-02-25 13:24:47 -0800

    organizing file structure

--- Project/PyPoll_Challenge.py
+++ Project/PyPoll_Challenge.py
@@ -3,9 +3,9 @@ import csv
 import os
 
 # add a variable to load a file from path
-file_to_load = os.path.join('Resources', 'election_results.csv')
+file_to_load = os.path.join('Project', 'Resources', 'election_results.csv')
 #print (File_to_load)
-file_to_save = os.path.join('Analysis','election_results.txt' )
+file_to_save = os.path.join('Project','Results','election_results.txt' )
 
 #Initialize a total vote counter
 total_votes = 0
